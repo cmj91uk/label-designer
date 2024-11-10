03e7524c8242c720739c1e499fed96db2fe148b8
Chris Jamison <4612668+cmj91uk@users.noreply.github.com>
2024-11-10 21:00:28 +0000

    Add google analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePluginRadar, VitePluginRadarOptions } from 'vite-plugin-radar'
+
+const radarOptions: VitePluginRadarOptions = {
+  enableDev: false,
+  analytics: {
+    id: 'G-EZWT2P78W7'
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      VitePluginRadar(radarOptions)
+  ],
   base: '/label-designer',
   css: {
     preprocessorOptions: {
