fe0f5f62b4a32bf2a4574eeda0cba4ecba147b8c
Chris Jamison <4612668+cmj91uk@users.noreply.github.com>
2025-03-16 21:18:50 +0000

    Linting errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
 import './app.css'
-import { ExampleForm } from './components/ExampleForm'
 import { LessonForm } from './components/LessonForm'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="min-h-screen bg-gray-900 py-12 px-4 sm:px-6">
       <LessonForm />
